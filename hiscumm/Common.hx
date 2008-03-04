6f408685aa276c2fc00bce168aeffa3d9874deb8
James Urquhart <jamesu@gmail.com>
2008-03-04 19:29:30 +0000

    Incorporate nekotest into core

--- hiscumm/Common.hx
+++ hiscumm/Common.hx
@@ -1,4 +1,11 @@
 package hiscumm;
+/*
+hiscumm
+-----------
+Copyright (C) 2007 - 2008 James S Urquhart (jamesu at gmail.com)
+Portions derived from code Copyright (C) 2004-2006 Alban Bedel
+This program is free software; you can redistribute it and/ormodify it under the terms of the GNU General Public Licenseas published by the Free Software Foundation; either version 2of the License, or (at your option) any later version.This program is distributed in the hope that it will be useful,but WITHOUT ANY WARRANTY; without even the implied warranty ofMERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See theGNU General Public License for more details.You should have received a copy of the GNU General Public Licensealong with this program; if not, write to the Free SoftwareFoundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
 
 #if flash9
 typedef ByteArray = flash.utils.ByteArray;
@@ -8,21 +15,27 @@ typedef Rectangle = flash.geom.Rectangle;
 typedef Point = flash.geom.Point;
 typedef Timer = flash.utils.Timer;
 typedef TimerEvent = flash.events.TimerEvent;
+typedef Input = noneko.Input;
+typedef Output = noneko.Output;
+typedef Int32 = noneko.Int32;
 #else neko
 typedef ByteArray = noflash.ByteArray;
-typedef Bitmap = noflash.ByteArray;
+typedef Bitmap = noflash.Bitmap;
 typedef BitmapData = noflash.BitmapData;
 typedef Rectangle = noflash.Rectangle;
 typedef Point = noflash.Point;
 typedef Timer = noflash.Timer;
 typedef TimerEvent = noflash.TimerEvent;
+typedef Int32 = neko.Int32;
 #else js
 typedef ByteArray = noflash.ByteArray;
-typedef ByteArray = noflash.ByteArray;
-typedef Bitmap = noflash.ByteArray;
+typedef Bitmap = noflash.Bitmap;
 typedef BitmapData = noflash.BitmapData;
 typedef Rectangle = noflash.Rectangle;
 typedef Point = noflash.Point;
 typedef Timer = noflash.Timer;
 typedef TimerEvent = noflash.TimerEvent;
+typedef Input = noneko.Input;
+typedef Output = noneko.Output;
+typedef Int32 = noneko.Int32;
 #end
--- hiscumm/SCUMM.hx
+++ hiscumm/SCUMM.hx
@@ -1,4 +1,4 @@
-package hiscumm;
+	package hiscumm;
 /*
 hiscumm
 -----------
