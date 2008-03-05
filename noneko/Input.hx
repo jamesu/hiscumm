5a371f2884d892acd58cedf6c35e2784607ca962
James Urquhart <jamesu@gmail.com>
2008-03-05 14:03:47 +0000

    Input and Output classes now interfaces

--- noneko/Input.hx
+++ noneko/Input.hx
@@ -7,183 +7,49 @@ Portions derived from code Copyright (C) 2005, The haXe Project Contributors
 This program is free software; you can redistribute it and/ormodify it under the terms of the GNU General Public Licenseas published by the Free Software Foundation; either version 2of the License, or (at your option) any later version.This program is distributed in the hope that it will be useful,but WITHOUT ANY WARRANTY; without even the implied warranty ofMERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See theGNU General Public License for more details.You should have received a copy of the GNU General Public Licensealong with this program; if not, write to the Free SoftwareFoundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
-class Input
+interface Input
 {
-	public f9dynamic function close() : Void
-	{
-		readBytes = function(_,_,_) { return throw Error.Closed; };
-		readChar = function() { return throw Error.Closed; };
-		close = function() { };
-	}
+	public function close() : Void;
 	
-	public function read(nbytes : Int) : String
-	{
-		return "";
-	}
+	public function read(nbytes : Int) : String;
 	
-	public function readAll(?bufsize : Int) : String
-	{
-		return "";
-	}
+	public function readAll(?bufsize : Int) : String;
 	
-	public f9dynamic function readBytes(s : String, p : Int, len : Int) : Int
-	{
-		return 0;
-	}
+	public function readBytes(s : String, p : Int, len : Int) : Int;
 	
-	public f9dynamic function readChar() : Int
-	{
-		return throw "Not implemented";
-	}
+	public function readChar() : Int;
 	
-	public function readDouble() : Float
-	{
-		return 0;
-	}
+	public function readDouble() : Float;
 	
-	public function readDoubleB() : Float
-	{
-		return 0;
-	}
+	public function readDoubleB() : Float;
 	
-	public function readFloat() : Float
-	{
-		return 0;
-	}
+	public function readFloat() : Float;
 	
-	public function readFloatB() : Float
-	{
-		return 0;
-	}
+	public function readFloatB() : Float;
 	
-	public function readFullBytes(s : String, pos : Int, len : Int) : Void
-	{
-		while( len > 0 ) {
-			var k = readBytes(s,pos,len);
-			pos += k;
-			len -= k;
-		}
-	}
+	public function readFullBytes(s : String, pos : Int, len : Int) : Void;
 	
-	public function readInt16() : Int
-	{
-		var ch1 = readChar();
-		var ch2 = readChar();
-		var n = ch1 | (ch2 << 8);
-		if( ch2 & 128 != 0 )
-			return n - 65536;
-		return n;
-	}
+	public function readInt16() : Int;
 	
-	public function readInt24() : Int
-	{
-		var ch1 = readChar();
-		var ch2 = readChar();
-		var ch3 = readChar();
-		var n = ch1 | (ch2 << 8) | (ch3 << 16);
-		if( ch3 & 128 != 0 )
-			return n - (1 << 24);
-		return n;
-	}
+	public function readInt24() : Int;
 	
-	public function readInt32() : Int
-	{
-		var ch1 = readChar();
-		var ch2 = readChar();
-		var ch3 = readChar();
-		var ch4 = readChar();
-		if( (ch4 & 128) != 0 ) {
-			if( ch4 & 64 == 0 ) throw Error.Overflow;
-			return ch1 | (ch2 << 8) | (ch3 << 16) | ((ch4 & 127) << 24);
-		} else {
-			if( ch4 & 64 != 0 ) throw Error.Overflow;
-			return ch1 | (ch2 << 8) | (ch3 << 16) | (ch4 << 24);
-		}
-	}
+	public function readInt32() : Int;
 	
-	public function readInt8() : Int
-	{
-		var n = readChar();
-		if( n >= 128 )
-			return n - 256;
-		return n;
-	}
+	public function readInt8() : Int;
 	
-	public function readLine() : String
-	{
-		var buf = new StringBuf();
-		var last : Int;
-		var s;
-		try {
-			while( (last = readChar()) != 10 )
-				buf.addChar( last );
-			s = buf.toString();
-			if( s.charCodeAt(s.length-1) == 13 ) s = s.substr(0,-1);
-		} catch( e : Eof ) {
-			s = buf.toString();
-			// TODO: fix for noneko
-			//if( s.length == 0 )
-			//	neko.Lib.rethrow(e);
-		}
-		return s;
-	}
+	public function readLine() : String;
 	
-	public function readUInt16() : Int
-	{
-		var ch1 = readChar();
-		var ch2 = readChar();
-		return ch1 | (ch2 << 8);
-	}
+	public function readUInt16() : Int;
 	
-	public function readUInt16B() : Int
-	{
-		var ch1 = readChar();
-		var ch2 = readChar();
-		return ch2 | (ch1 << 8);
-	}
+	public function readUInt16B() : Int;
 	
-	public function readUInt24() : Int
-	{
-		var ch1 = readChar();
-		var ch2 = readChar();
-		var ch3 = readChar();
-		return ch1 | (ch2 << 8) | (ch3 << 16);
-	}
+	public function readUInt24() : Int;
 	
-	public function readUInt24B() : Int
-	{
-		var ch1 = readChar();
-		var ch2 = readChar();
-		var ch3 = readChar();
-		return ch3 | (ch2 << 8) | (ch1 << 16);
-	}
+	public function readUInt24B() : Int;
 	
-	public function readUInt32() : Int
-	{
-		var ch1 = readChar();
-		var ch2 = readChar();
-		var ch3 = readChar();
-		var ch4 = readChar();
-		if( ch4 >= 64 ) throw Error.Overflow;
-		return ch1 | (ch2 << 8) | (ch3 << 16) | (ch4 << 24);
-	}
+	public function readUInt32() : Int;
 	
-	public function readUInt32B() : Int
-	{
-		var ch1 = readChar();
-		var ch2 = readChar();
-		var ch3 = readChar();
-		var ch4 = readChar();
-		if( ch1 >= 64 ) throw Error.Overflow;
-		return ch4 | (ch3 << 8) | (ch2 << 16) | (ch1 << 24);
-	}
+	public function readUInt32B() : Int;
 	
-	public function readUntil(end : Int) : String
-	{
-		var buf = new StringBuf();
-		var last : Int;
-		while( (last = readChar()) != end )
-			buf.addChar( last );
-		return buf.toString();
-	}
+	public function readUntil(end : Int) : String;
 }
--- noneko/Output.hx
+++ noneko/Output.hx
@@ -8,150 +8,49 @@ Portions derived from code Copyright (C) 2004-2006 Alban Bedel,
 This program is free software; you can redistribute it and/ormodify it under the terms of the GNU General Public Licenseas published by the Free Software Foundation; either version 2of the License, or (at your option) any later version.This program is distributed in the hope that it will be useful,but WITHOUT ANY WARRANTY; without even the implied warranty ofMERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See theGNU General Public License for more details.You should have received a copy of the GNU General Public Licensealong with this program; if not, write to the Free SoftwareFoundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
-class Output
+interface Output
 {
-	public f9dynamic function close() : Void
-	{
-		writeBytes = function(_,_,_) { return throw Error.Closed; };
-		writeChar = function(_) { throw Error.Closed; };
-		flush = function() { throw Error.Closed; };
-		close = function() { };
-	}
+	public function close() : Void;
 	
-	public f9dynamic function flush() : Void
-	{
-	}
+	public function flush() : Void;
 	
-	public function prepare(nbytes : Int) : Void
-	{
-	}
+	public function prepare(nbytes : Int) : Void;
 	
-	public function write(s : String) : Void
-	{
-		var l = s.length;
-		var p = 0;
-		while( l > 0 ) {
-			var k = writeBytes(s,p,l);
-			if( k == 0 ) throw Error.Blocked;
-			p += k;
-			l -= k;
-		}
-	}
+	public function write(s : String) : Void;
 	
-	public f9dynamic function writeBytes(s : String, p : Int, len : Int) : Int
-	{
-		return 0;
-	}
+	public function writeBytes(s : String, p : Int, len : Int) : Int;
 	
-	public f9dynamic function writeChar(c : Int) : Void
-	{
-		throw "Not implemented";
-	}
+	public function writeChar(c : Int) : Void;
 	
-	public function writeDouble(c : Float) : Void
-	{
-	}
+	public function writeDouble(c : Float) : Void;
 	
-	public function writeDoubleB(c : Float) : Void
-	{
-	}
+	public function writeDoubleB(c : Float) : Void;
 	
-	public function writeFloat(c : Float) : Void
-	{
-	}
+	public function writeFloat(c : Float) : Void;
 	
-	public function writeFloatB(c : Float) : Void
-	{
-	}
+	public function writeFloatB(c : Float) : Void;
 	
-	public function writeFullBytes(s : String, pos : Int, len : Int) : Void
-	{
-		while( len > 0 ) {
-			var k = writeBytes(s,pos,len);
-			pos += k;
-			len -= k;
-		}
-	}
+	public function writeFullBytes(s : String, pos : Int, len : Int) : Void;
 	
-	public function writeInput(i : Input, ?bufsize : Int) : Void
-	{
-	}
+	public function writeInput(i : Input, ?bufsize : Int) : Void;
 	
-	public function writeInt16(x : Int) : Void
-	{
-		if( x < -0x8000 || x > 0x7FFF ) throw Error.Overflow;
-		if( x < 0 )
-			writeUInt16(0x10000 + x);
-		else
-			writeUInt16(x);
-	}
+	public function writeInt16(x : Int) : Void;
 	
-	public function writeInt24(x : Int) : Void
-	{
-		if( x < 0 || x > 0xFFFFFF ) throw Error.Overflow;
-		writeChar(x & 0xFF);
-		writeChar((x >> 8) & 0xFF);
-		writeChar(x >> 16);
-	}
+	public function writeInt24(x : Int) : Void;
 	
-	public function writeInt32(x : Int) : Void
-	{
-		writeChar(x & 0xFF);
-		writeChar((x >> 8) & 0xFF);
-		writeChar((x >> 16) & 0xFF);
-		writeChar(x >>> 24);
-	}
+	public function writeInt32(x : Int) : Void;
 	
-	public function writeInt8(c : Int) : Void
-	{
-		if( c < -0x80 || c > 0x7F )
-			throw Error.Overflow;
-		writeChar(c & 0xFF);
-	}
+	public function writeInt8(c : Int) : Void;
 	
-	public function writeUInt16(x : Int) : Void
-	{
-		if( x < 0 || x > 0xFFFF ) throw Error.Overflow;
-		writeChar(x & 0xFF);
-		writeChar(x >> 8);
-	}
+	public function writeUInt16(x : Int) : Void;
 	
-	public function writeUInt16B(x : Int) : Void
-	{
-		if( x < 0 || x > 0xFFFF ) throw Error.Overflow;
-		writeChar(x >> 8);
-		writeChar(x & 0xFF);
-	}
+	public function writeUInt16B(x : Int) : Void;
 	
-	public function writeUInt24(x : Int) : Void
-	{
-		if( x < -0x800000 || x > 0x7FFFFF ) throw Error.Overflow;
-		if( x < 0 )
-			writeUInt24(0x1000000 + x);
-		else
-			writeUInt24(x);
-	}
+	public function writeUInt24(x : Int) : Void;
 	
-	public function writeUInt24B(x : Int) : Void
-	{
-		if( x < 0 || x > 0xFFFFFF ) throw Error.Overflow;
-		writeChar(x >> 16);
-		writeChar((x >> 8) & 0xFF);
-		writeChar(x & 0xFF);
-	}
+	public function writeUInt24B(x : Int) : Void;
 	
-	public function writeUInt32(x : Int) : Void
-	{
-		if( x < 0 ) throw Error.Overflow;
-		writeInt32(x);
-	}
+	public function writeUInt32(x : Int) : Void;
 	
-	public function writeUInt32B(x : Int) : Void
-	{
-		if( x < 0 ) throw Error.Overflow;
-		writeChar(x >>> 24);
-		writeChar((x >> 16) & 0xFF);
-		writeChar((x >> 8) & 0xFF);
-		writeChar(x & 0xFF);
-	}
+	public function writeUInt32B(x : Int) : Void;
 }
