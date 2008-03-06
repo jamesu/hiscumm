0d11622d545b1414fdc7a1be8b3aac12cd0a9b85
James Urquhart <jamesu@gmail.com>
2008-03-06 11:26:38 +0000

    Add new classes
    - FlashByteIO class
    - Seekable interface

---
+++ utils/Seekable.hx
@@ -0,0 +1,32 @@
+package utils;
+/*
+hiscumm
+-----------
+Copyright (C) 2008 James S Urquhart (jamesu at gmail.com)
+This program is free software; you can redistribute it and/ormodify it under the terms of the GNU General Public Licenseas published by the Free Software Foundation; either version 2of the License, or (at your option) any later version.This program is distributed in the hope that it will be useful,but WITHOUT ANY WARRANTY; without even the implied warranty ofMERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See theGNU General Public License for more details.You should have received a copy of the GNU General Public Licensealong with this program; if not, write to the Free SoftwareFoundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+enum Seek
+{
+	SeekEnd;
+	SeekCur;
+	SeekBegin;
+}
+
+interface Seekable
+{	
+	function seek(p : Int, pos : Seek) : Void;
+	function tell() : Int;
+}
+
+class SeekableTools
+{
+	public static function getSeekableLength(s : Seekable) : Int
+	{
+		var old_pos = s.tell();
+		s.seek(0, SeekEnd);
+		var new_pos = s.tell();
+		s.seek(old_pos, SeekBegin);
+		return new_pos;
+	}
+}
